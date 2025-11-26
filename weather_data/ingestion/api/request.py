055efbc2208a6fd21f0137e5ac3ec0e7183f8399
brichlerd <brichlerd@gmail.com>
2025-11-26 12:18:42 -0500

    api pull into dfs

--- weather_data/ingestion/api/request.py
+++ weather_data/ingestion/api/request.py
@@ -19,18 +19,6 @@ class WeatherRequest:
         self.unit_group = "unitGroup=us"
         self.content_type="contentType=json"
 
-    def get_current_weather(self, query: str):
-        params = {
-            "endpoint": "current",
-            "access_key": self.access_key,
-            "query": query
-        }
-        # url = base_url + page + access_key + query
-        url = f"{self.base_url}/{params.get('endpoint')}?access_key={params.get('access_key')}&query={params.get('query')}"
-        response = requests.get(url)
-        # return url
-        return response.json()
-
     # https: // weather.visualcrossing.com / VisualCrossingWebServices / rest / services / timeline
     # / 43205?unitGroup = us & key = (QWF4ZLNFTSVHNHMV9WBS7W34F
     # & contentType) = json
@@ -48,8 +36,4 @@ class WeatherRequest:
             logger.exception(e)
 
         response = requests.get(url)
-        # return print(url)
-        # return date_list
         return response.json()
-
-WeatherRequest().get_historical_weather("Ohio&historical_date=2025-11-23")
---
+++ weather_data/ingestion/processing/weather.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import json
+
+from weather_data.ingestion.api.request import WeatherRequest
+
+def pull_weather_data(zip_code: str) -> None:
+    raw_json = WeatherRequest().get_historical_weather("43205")
+    # pick base flat json objects/columns & add to a dataframe
+    base_field_dict = {
+        "location": "resolvedAddress",
+        "timezone": "timezone",
+        "timezone_offset": "tzoffset",
+        "current_description": "description"
+    }
+
+    # flatten the currentConditions (dict) and days (list of dicts) objects
+    current = pd.json_normalize(raw_json["currentConditions"])
+    forecast = pd.json_normalize(raw_json["days"])
+
+    df_base = pd.DataFrame()
+    # append the json objects in base_field_dict (values) to the dataframe naming columns as base_field_dict (keys)
+    for k, v in base_field_dict.items():
+        df_base[k] = [raw_json.get(v)]
+
+    # merge the current dataframe to the base dataframe on index
+    df_current = df_base.merge(current, left_index=True, right_index=True, how="left", suffixes=("","_current"))
+
+    # if column name contains Epoch convert to datetime
+    epoch_cols = df_current.columns[df_current.columns.str.contains('Epoch')]
+    df_current[epoch_cols] = df_current[epoch_cols].apply(pd.to_datetime, unit='s')
+
+    # merge the forecast dataframe to the base dataframe so each forecast day is a new row
+    df_forecast = df_base.merge(forecast, how="cross", suffixes=("","_forecast"))
+
+    # if column name contains Epoch convert to datetime
+    epoch_cols = df_current.columns[df_current.columns.str.contains('Epoch')]
+    df_forecast[epoch_cols] = df_forecast[epoch_cols].apply(pd.to_datetime, unit='s')
+
+pull_weather_data("43205")
